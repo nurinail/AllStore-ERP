820ffb06bece446f87fd82c4204127d5e59796da
Nail Nuri <nurinail2003@gmail.com>
2025-08-24 20:05:06 +0400

    netlify deploy proje

--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,7 @@
 import { defineConfig } from 'vite'
 import reactPlugin from '@vitejs/plugin-react'
 
-// Manual code-splitting (istəyə bağlı)
 export default defineConfig({
   plugins: [reactPlugin()],
-  base: '/AllStore-ERP/',
-  build: {
-    rollupOptions: {
-      output: {
-        manualChunks: {
-          'react-vendor': ['react', 'react-dom', 'react-router-dom'],
-          'antd-vendor': ['antd']
-        }
-      }
-    },
-    chunkSizeWarningLimit: 1200
-  }
+  
 })
